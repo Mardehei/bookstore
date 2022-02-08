410cc282a3cc502b993608a1e2ee9d8694f0aec6
Mardehei <82866317+Mardehei@users.noreply.github.com>
2022-02-08 17:43:26 +0200

    tehtävä valmis

--- src/main/java/hh22/bookstore/web/BookController.java
+++ src/main/java/hh22/bookstore/web/BookController.java
@@ -3,8 +3,10 @@ package hh22.bookstore.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ResponseBody // tämä lähtee kun tehtävää viedään eteenpäin ja tehdään html-template
 public class BookController {
 
 	@RequestMapping(value = "/index", method = RequestMethod.GET) 
